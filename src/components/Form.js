54f922cb050d4cf399049759cdc270dc0b496d06
Jose Miguel Valiente Niño <valiente_jose2@hotmail.com>
2020-02-26 18:01:55 +0100

    init

---
+++ src/components/Form.js
@@ -0,0 +1,40 @@
+import React, {Fragment} from 'react'
+
+const Form = () => {
+    return(
+        <Fragment>
+
+            <div className='container'>
+                <form className='col s12'>
+                    <div className="row">
+                        <div className="input-field col s6">
+                            <input id="first_name" type="text" className="validate"/>
+                            <label htmlFor="first_name">Numero Transpalet</label>
+                        </div>
+                    </div>
+                    <div className='row'>    
+                        <div className="input-field col s6">
+                            <input id="last_name" type="text" className="validate"/>
+                            <label htmlFor="last_name">Proveedor</label>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div class="switch">
+                            <label>
+                            Activar RFID: Off
+                            <input type="checkbox"/>
+                            <span class="lever"></span>
+                            On
+                            </label>
+                        </div>
+                    </div>
+
+                </form>
+
+            </div>
+        </Fragment>
+        
+    )
+}
+
+export default Form
